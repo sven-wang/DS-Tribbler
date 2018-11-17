b740e9940588db67dd1064744880cfe86b1ac452
WinttterZ <lwzhong@yahoo.com>
2018-11-17 14:37:40 -0500

    storage server init

--- src/github.com/cmu440/tribbler/storageserver/storageserver_impl.go
+++ src/github.com/cmu440/tribbler/storageserver/storageserver_impl.go
@@ -2,12 +2,30 @@ package storageserver
 
 import (
 	"errors"
-
+	"fmt"
 	"github.com/cmu440/tribbler/rpc/storagerpc"
+	"net"
+	"net/http"
+	"net/rpc"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type Role int
+
+const (
+	MASTER Role = 0
+	SLAVE  Role = 1
 )
 
 type storageServer struct {
-	// TODO: implement this!
+	role       Role                       // Role of this storage server - MASTER or SLAVE
+	registerChan chan storagerpc.Node     // Channel used for register servers
+	mux			sync.Mutex				  // Lock for this storage server
+	numNodes   int						  // Number of servers in the hash ring
+	allServers map[string]storagerpc.Node // All the storage servers in the system
+	virtualIDs []uint32                   // Virtual IDs assigned to this server
 }
 
 // NewStorageServer creates and starts a new StorageServer. masterServerHostPort
@@ -19,11 +37,98 @@ type storageServer struct {
 // This function should return only once all storage servers have joined the ring,
 // and should return a non-nil error if the storage server could not be started.
 func NewStorageServer(masterServerHostPort string, numNodes, port int, virtualIDs []uint32) (StorageServer, error) {
-	return nil, errors.New("not implemented")
+	ss := new(storageServer)
+	ss.virtualIDs = virtualIDs
+	ss.numNodes = numNodes
+	ss.allServers = make(map[string]storagerpc.Node)
+
+	// Create the server socket that will listen for incoming RPCs.
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+
+	// Wrap the storageServer before registering it for RPC.
+	err = rpc.RegisterName("StorageServer", storagerpc.Wrap(ss))
+	if err != nil {
+		return nil, err
+	}
+
+	// Setup the HTTP handler that will server incoming RPCs and
+	// serve requests in a background goroutine.
+	rpc.HandleHTTP()
+	go http.Serve(listener, nil)
+
+	// TODO: Own IP address?
+	ownHostAddr := net.JoinHostPort("localhost", strconv.Itoa(port))
+	// If the server is the MASTER server
+	if len(masterServerHostPort) == 0 {
+		ss.role = MASTER
+		ss.registerChan = make(chan storagerpc.Node)
+
+		// MASTER registers itself first
+		ss.allServers[ownHostAddr] = storagerpc.Node{HostPort: ownHostAddr, VirtualIDs: virtualIDs}
+		for {
+			select {
+			case newNode := <-ss.registerChan:
+				ss.mux.Lock()
+				if _, ok := ss.allServers[newNode.HostPort]; !ok {
+					ss.allServers[newNode.HostPort] = newNode
+					// All servers have registered
+					if len(ss.allServers) == ss.numNodes {
+						ss.mux.Unlock()
+						break
+					}
+				}
+				ss.mux.Unlock()
+			}
+		}
+	} else {
+		ss.role = SLAVE
+		// SLAVE server ask the MASTER to add itself into the ring
+		client, err := rpc.DialHTTP("tcp", net.JoinHostPort(masterServerHostPort, strconv.Itoa(port)))
+		if err != nil {
+			return nil, err
+		}
+
+		args := &storagerpc.RegisterArgs{ServerInfo: storagerpc.Node{HostPort: ownHostAddr, VirtualIDs: virtualIDs}}
+		reply := &storagerpc.RegisterReply{}
+		// Retry no more than 5 times
+		for {
+			err = client.Call("StorageServer.RegisterServer", args, reply)
+			if err != nil {
+				return nil, err
+			}
+			if reply.Status == storagerpc.NotReady {
+				time.Sleep(1 * time.Second)
+			} else if reply.Status == storagerpc.OK {
+				for _, nodeInfo := range reply.Servers {
+					ss.allServers[nodeInfo.HostPort] = nodeInfo
+				}
+				break
+			}
+		}
+	}
+
+	return ss, nil
 }
 
 func (ss *storageServer) RegisterServer(args *storagerpc.RegisterArgs, reply *storagerpc.RegisterReply) error {
-	return errors.New("not implemented")
+	ss.mux.Lock()
+	if len(ss.allServers) == ss.numNodes {
+		reply.Status = storagerpc.OK
+		var registeredServers []storagerpc.Node
+		for _, nodeInfo := range ss.allServers {
+			registeredServers = append(registeredServers, nodeInfo)
+		}
+		reply.Servers = registeredServers
+		ss.mux.Unlock()
+	} else {
+		ss.mux.Unlock()
+		ss.registerChan <- args.ServerInfo
+		reply.Status = storagerpc.NotReady
+	}
+	return nil
 }
 
 func (ss *storageServer) GetServers(args *storagerpc.GetServersArgs, reply *storagerpc.GetServersReply) error {
